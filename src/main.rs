98057887a5e5d74050223c4a0e284ee3662eefdf
Leonard Excoffier <48970393+excoffierleonard@users.noreply.github.com>
2024-11-22 19:31:23 -0500

    feat: most basic working example of route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Response {
+    message: String,
+}
+
+#[get("/v1/test")]
+async fn test() -> impl Responder {
+    let response = Response {
+        message: "Test Worked".to_string(),
+    };
+    web::Json(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server starting on http://0.0.0.0:8080");
+
+    HttpServer::new(|| App::new().service(test))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
